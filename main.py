fe9119de9d5c5129c0011abf43fbc5869150088f
eee46666 <quocgm1@icloud.com>
2026-03-15 13:52:20 +0700

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T06:51:10.522Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-15T06:52:19.741Z
+Commit: Add multi-language support
