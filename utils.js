6fbeffffa637b7ac77a88743344b0ea123114f15
eee46666 <quocgm1@icloud.com>
2026-03-15 13:50:46 +0700

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-15T06:50:18.936Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-15T06:50:45.283Z
+Commit: Resolve timezone issues in date handling
